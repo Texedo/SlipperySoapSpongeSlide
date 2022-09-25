12554fab799146f1a96985b1a78ee192f913f113
Texedo <techzombie12345@gmail.com>
2022-09-25 19:17:05 -0400

    Added Player and TrackSegment scripts

---
+++ Assets/Scripts/TrackSegment.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackSegment : MonoBehaviour
+{
+  public List<GameObject> surfaceMarkers;
+  public List<int> markerContinuationIndices;
+}
